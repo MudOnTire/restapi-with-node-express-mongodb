33f6d982b2fecb5f177a1db09b75969281c442ba
MudOnTire <895157882@qq.com>
2018-09-28 15:47:46 +0800

    add front page

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ mongoose.connect('mongodb://localhost/restapi-demo', {
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
 
+app.use(express.static('public'));
+
 app.use(bodyParser.json());
 
 app.use('/api', router);
